e6256e0a60a45c83930071d1c7a965ae0d08f711
Alexandr <semakaleksandr2014@gmail.com>
2018-07-10 10:29:51 +0300

    func edit name - ready!

--- src/components/contactInfo.jsx
+++ src/components/contactInfo.jsx
@@ -6,27 +6,62 @@ import { updateContact } from "../actions/actionContacts";
 
 class ContactInfo extends Component {
 
+  state = {
+    edit: false,
+    nameEdit: ""
+  }
+
   handleRemoveClick = (removeContact) => {    
     this.props.removeContact(removeContact);
   };
 
-  handleUpdateClick = (id, data) => {    
-    this.props.updateContact(id, {name: "Alex"});
-    console.log('update', id, data)
+  editContact = () => {
+    this.setState({
+      edit: true
+    });
+  }
+
+  nameEditChange = (e) => {
+    this.setState({
+      nameEdit: e.target.value,      
+    });
+  }  
+
+  handleUpdateClick = (id, data) => { 
+
+    this.setState({
+      edit: true      
+    });  
+
+    this.handleSave = () => {
+      this.setState({
+        edit: false      
+      }); 
+
+      data.name = this.state.nameEdit;
+      this.props.updateContact(id, data);
+    }    
+   
   };
 
   render() {
+
+    const start = <p>
+                    <span> name: {this.props.name} </span><br/>         
+                    <span> telNum: {this.props.telNum} </span>
+                    <button onClick={() => this.handleRemoveClick(this.props.contactId)} >&times;</button>
+                    <button onClick={() => this.handleUpdateClick(this.props.contactId, {name: this.props.name, phone: this.props.telNum } )} >Edit</button>                             
+                  </p>;
+    const editInputs = <div>
+                        <input type="text" value={this.state.nameEdit} onChange={this.nameEditChange} />
+                        <button onClick={this.handleSave}>Save</button> 
+                      </div>;
+
     return (
       <div>
 
-        <p>
-          <span> name: {this.props.name} </span><br/>         
-          <span> telNum: {this.props.telNum} </span>
-          <button onClick={() => this.handleRemoveClick(this.props.contactId)} >&times;</button>
-
-          <button onClick={() => this.handleUpdateClick(this.props.contactId, {name: this.props.name, phone: this.props.telNum } )} >Edit</button>
-         
-        </p>        
+        { !this.state.edit ? start : editInputs}       
+                
         <hr/>
 
       </div>
