41d7b19435b897749badca76adbbf5322d612ed1
Alexandr <semakaleksandr2014@gmail.com>
2018-07-10 12:22:33 +0300

    edit name and edit phone READY!

--- src/components/contactInfo.jsx
+++ src/components/contactInfo.jsx
@@ -8,7 +8,8 @@ class ContactInfo extends Component {
 
   state = {
     edit: false,
-    nameEdit: ""
+    nameEdit: "",
+    phoneEdit: ""
   }
 
   handleRemoveClick = (removeContact) => {    
@@ -22,15 +23,23 @@ class ContactInfo extends Component {
   }
 
   nameEditChange = (e) => {
-    this.setState({
-      nameEdit: e.target.value,      
+   this.setState({            
+      nameEdit: e.target.value      
     });
-  }  
+  } 
+
+  phoneEditChange = (e) => {
+    this.setState({            
+      phoneEdit: e.target.value      
+    });
+  }
 
   handleUpdateClick = (id, data) => { 
 
     this.setState({
-      edit: true      
+      edit: true,
+      nameEdit: data.name,
+      phoneEdit: data.phone      
     });  
 
     this.handleSave = () => {
@@ -39,6 +48,7 @@ class ContactInfo extends Component {
       }); 
 
       data.name = this.state.nameEdit;
+      data.phone = this.state.phoneEdit;
       this.props.updateContact(id, data);
     }    
    
@@ -53,15 +63,15 @@ class ContactInfo extends Component {
                     <button onClick={() => this.handleUpdateClick(this.props.contactId, {name: this.props.name, phone: this.props.telNum } )} >Edit</button>                             
                   </p>;
     const editInputs = <div>
-                        <input type="text" value={this.state.nameEdit} onChange={this.nameEditChange} />
+                        <input type="text" name="name" value={this.state.nameEdit} onChange={this.nameEditChange} />
+                        <input type="tel" name="tel" value={this.state.phoneEdit} onChange={this.phoneEditChange} />
                         <button onClick={this.handleSave}>Save</button> 
                       </div>;
 
     return (
       <div>
 
-        { !this.state.edit ? start : editInputs}       
-                
+        { !this.state.edit ? start : editInputs }                
         <hr/>
 
       </div>
